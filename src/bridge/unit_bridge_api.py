81bd47ed5472d9aed232764906dab135ce43685b
mfnatiq <wotisup@protonmail.com>
2025-08-18 08:37:01 +0800

    remove headers

--- src/bridge/unit_bridge_api.py
+++ src/bridge/unit_bridge_api.py
@@ -12,12 +12,6 @@ class UnitBridgeInfo():
         self.session = requests.Session()
         self.session.headers.update({
             "Content-Type": "application/json",
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
-            'Accept': 'application/json, text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
         })
         self._logger = logging.getLogger(__name__)
 
