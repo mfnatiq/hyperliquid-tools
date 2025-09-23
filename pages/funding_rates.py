0f56f613af8bd6354ac7b7e02b60eee1a02bfa95
mfnatiq <wotisup@protonmail.com>
2025-09-23 23:34:10 +0800

    add bybit basic

--- pages/funding_rates.py
+++ pages/funding_rates.py
@@ -1,6 +1,3 @@
-from hyperliquid.info import Info
-from hyperliquid.utils import constants
-from hyperliquid.utils.error import ClientError, ServerError
 import pandas as pd
 import requests
 import streamlit as st
@@ -11,7 +8,7 @@ st.set_page_config(
     layout="wide",
 )
 
-st.title("Funding Rate Comparison")
+st.title("Funding Rate Comparison (1H)")
 
 # setup and configure logging
 import logging
@@ -21,34 +18,40 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# hyperliquid
-try:
-    hyperliquid_info = Info(constants.MAINNET_API_URL, skip_ws=True)
-except Exception as e:
-    logger.error(f'client error occured: {e}')
-    st.error(
-        "Hyperliquid API rate limit reached, please try again in a short while")
+fetch_funding_rates_cache_duration_s = 60
 
-# TODO handle rate limit issues
-@st.cache_data(ttl=60, show_spinner=False)
-def get_hyperliquid_funding_rates():
-    return hyperliquid_info.meta_and_asset_ctxs()
-
-@st.cache_data(ttl=60, show_spinner=False)
+@st.cache_data(ttl=fetch_funding_rates_cache_duration_s, show_spinner=False)
 def get_lighter_funding_rates():
+    # contains binance, bybit, hyperliquid, lighter
     response = requests.get(
         "https://mainnet.zklighter.elliot.ai/api/v1/funding-rates",
         headers={ "accept": "application/json" },
     )
     return response.json()
 
-@st.cache_data(ttl=60, show_spinner=False)
+@st.cache_data(ttl=fetch_funding_rates_cache_duration_s, show_spinner=False)
 def get_pacifica_funding_rates():
     response = requests.get(
         "https://api.pacifica.fi/api/v1/info",
         headers={ "Accept": "*/*" },
     )
     return response.json()
+
+@st.cache_data(ttl=fetch_funding_rates_cache_duration_s, show_spinner=False)
+def get_pacifica_funding_rates():
+    response = requests.get(
+        "https://api.pacifica.fi/api/v1/info",
+        headers={ "Accept": "*/*" },
+    )
+    return response.json()
+
+# @st.cache_data(ttl=fetch_funding_rates_cache_duration_s, show_spinner=False)
+# def get_variational_funding_rates():
+#     response = requests.get(
+#         "https://api.variational.io/v1/metadata/supported_assets",
+#         headers={ "Accept": "*/*" },
+#     )
+#     return response.json()
 
 # --- 2. Styling Function ---
 def apply_styles(val):
@@ -63,14 +66,14 @@ def apply_styles(val):
     if pd.isna(val):
         # Use a muted grey for NaN values
         return 'color: #808080'
-    
+
     # Now check for numeric types
     if isinstance(val, (int, float)):
         if val > 0:
             return 'color: #2E8B57'  # Green
         elif val < 0:
             return 'color: #C70039'  # Red
-            
+
     # Return no specific style for other cases (like zero or text)
     return ''
 
@@ -79,6 +82,7 @@ with st.spinner(show_time=True):
 
     lighter_funding_rates = get_lighter_funding_rates()['funding_rates']
     pacifica_funding_rates = get_pacifica_funding_rates()['data']
+    # variational_funding_rates = get_variational_funding_rates()['result']
 
     # Create a DataFrame from the first list of data
     df1 = pd.DataFrame(lighter_funding_rates)
@@ -95,8 +99,24 @@ with st.spinner(show_time=True):
     # Merge the two DataFrames on the 'symbol'
     combined_df = pd.merge(funding_rates_df, pacifica_df, on='symbol', how='outer')
 
+    # # --- 3. PARSE AND PROCESS VARIATIONAL DATA ---
+    # parsed_variational = []
+    # for symbol, data_list in variational_funding_rates.items():
+    #     if data_list:
+    #         params = data_list[0].get('variational_funding_rate_params', {})
+    #         normal_slope = params.get('normal_slope')
+    #         if normal_slope is not None:
+    #             parsed_variational.append({
+    #                 'symbol': symbol,
+    #                 'variational': float(normal_slope)
+    #             })
+    # variational_df = pd.DataFrame(parsed_variational)
+    # breakpoint()
+
+    # final_df = pd.merge(combined_df, variational_df, on='symbol', how='outer')
+
     # Define the desired column order
-    numeric_cols = ['binance', 'hyperliquid', 'lighter', 'pacifica']
+    numeric_cols = ['binance', 'bybit', 'hyperliquid', 'lighter', 'pacifica']
     final_columns = ['symbol'] + numeric_cols
 
     # Reindex the DataFrame to include all desired columns, filling missing ones with NaN
@@ -108,7 +128,7 @@ with st.spinner(show_time=True):
 
     # 3. Group by 'base_symbol' and aggregate the results for exchange columns
     # The .first() method takes the first non-null value for each exchange in the group.
-    exchange_cols = ['binance', 'hyperliquid', 'lighter', 'pacifica']
+    exchange_cols = ['binance', 'bybit', 'hyperliquid', 'lighter', 'pacifica']
     merged_df = combined_df.groupby('base_symbol')[exchange_cols].first()
 
     # 4. Finalize the DataFrame
@@ -121,12 +141,21 @@ with st.spinner(show_time=True):
     # 5. Drop rows where ALL exchange values are NaN
     final_df = final_df.dropna(subset=exchange_cols, how='all')
 
+    # lighter api returns 8h rates but pacifica api returns 1h rates
+    formatters = {
+        'binance': lambda x: f"{(x / 8) * 100:.4f}%",
+        'bybit': lambda x: f"{(x / 8) * 100:.4f}%",
+        'hyperliquid': lambda x: f"{(x / 8) * 100:.4f}%",
+        'lighter': lambda x: f"{(x / 8) * 100:.4f}%",
+        'pacifica': lambda x: f"{x * 100:.4f}%"
+    }
+
     st.dataframe(
         final_df.style
             # Apply the color styling function first
             .map(apply_styles, subset=exchange_cols)
             # Use .format() ONLY to change the display text for NaNs
-            .format(na_rep="---", subset=exchange_cols),
+            .format(formatters, na_rep="---", subset=exchange_cols),
         hide_index=True,
         # column_config={
         #     'user_rank': st.column_config.TextColumn('Rank'),
@@ -135,6 +164,4 @@ with st.spinner(show_time=True):
         # },
     )
 
-    # TODO convert all to percentages and mention is hourly
-    # for all binance / hyperliquid / lighter, divide by 8 then x100 then add %
-    # for pacifica, x100 then add % (alr in 1h)
+    # TODO check any pagination needed for any apis (variational?)
