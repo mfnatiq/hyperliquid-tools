36a2f5688779bfc5aaa6fba9eac0f950b8f51753
mfnatiq <wotisup@protonmail.com>
2026-06-03 16:50:42 +0800

    fix add missing file

---
+++ src/hl_client.py
@@ -0,0 +1,26 @@
+import requests
+from hyperliquid.utils import constants
+
+class HyperliquidClient:
+    def post(self, endpoint: str, payload: dict):
+        resp = requests.post(f'{constants.MAINNET_API_URL}/{endpoint}', json=payload)
+        resp.raise_for_status()
+        return resp.json()
+
+    def spot_meta(self) -> dict:
+        return self.post("/info", {"type": "spotMeta"})
+
+    def meta(self, dex: str = "") -> dict:
+        return self.post("/info", {"type": "meta", "dex": dex})
+
+    def all_mids(self) -> dict:
+        return self.post("/info", {"type": "allMids"})
+
+    def query_sub_accounts(self, user: str) -> list:
+        return self.post("/info", {"type": "subAccounts", "user": user})
+
+    def candles_snapshot(self, name: str, interval: str, startTime: int, endTime: int) -> list:
+        return self.post("/info", {
+            "type": "candleSnapshot",
+            "req": {"coin": name, "interval": interval, "startTime": startTime, "endTime": endTime},
+        })
